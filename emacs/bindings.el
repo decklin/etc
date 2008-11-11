2a54b0a4dd63c6ddd012e86b59880e049e853c75
Decklin Foster <decklin@red-bean.com>
2008-11-11 00:24:40 -0500

    Screw it, I keep hitting this by accident

--- emacs/bindings.el
+++ emacs/bindings.el
@@ -1,6 +1,7 @@
 (global-set-key (kbd "C-x M-f") 'find-file-at-point)
 (global-set-key (kbd "C-x C-b") 'electric-buffer-list)
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
+(global-set-key (kbd "C-x M-s") 'speedbar-positioned)
 
 (global-set-key (kbd "M-n") 'cyclebuffer-forward)
 (global-set-key (kbd "M-p") 'cyclebuffer-backward)
@@ -11,5 +12,3 @@
 
 (global-set-key (kbd "M-;") 'comment-toggle-line)
 (global-set-key (kbd "C-;") 'comment-indent)
-
-(global-set-key (kbd "S-SPC") 'speedbar-positioned)
