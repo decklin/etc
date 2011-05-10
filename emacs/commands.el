0929c44e3772366fa967c07d3a70d961965f4624
Decklin Foster <decklin@red-bean.com>
2011-05-10 08:30:07 -0400

    Fix comment-toggle-line in transient mark mode

--- emacs/commands.el
+++ emacs/commands.el
@@ -2,12 +2,14 @@
 
 (defun comment-toggle-line (arg)
   (interactive "p")
-  (save-excursion
-    (beginning-of-line)
-    (let ((beg (point)))
-      (forward-line arg)
-      (backward-char)
-      (comment-or-uncomment-region beg (point)))))
+  (if (and mark-active transient-mark-mode)
+      (comment-or-uncomment-region (region-beginning) (region-end) arg)
+    (save-excursion
+      (beginning-of-line)
+      (let ((beg (point)))
+        (forward-line arg)
+        (backward-char)
+        (comment-or-uncomment-region beg (point))))))
 
 ;; oh my god, i hate everyone. read the motherfucking ICCCM.
 
