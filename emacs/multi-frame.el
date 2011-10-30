1c2ec18f1ed4328d8d413e5afced51b63a81c48e
Decklin Foster <decklin@red-bean.com>
2011-10-30 14:39:41 -0400

    Rename 'dark' color theme to 'tty' and tweak

--- emacs/multi-frame.el
+++ emacs/multi-frame.el
@@ -1,6 +1,6 @@
 ;; This lets us start on a tty, X11 frame, or (preferably) as a daemon
 ;; that will create either on demand (possibly at the same time), with
-;; the right color theme for each (light for X11, dark for tty).
+;; the right color theme for each.
 ;;
 ;; tool-bar-mode should be in modes.el, not here, but it misbehaves
 ;; and turns itself on again every time we make a new frame.
@@ -12,7 +12,7 @@
         (when-bound (color-theme-decklin-light))
         (menu-bar-mode 1))
     (progn
-      (when-bound (color-theme-decklin-dark))
+      (when-bound (color-theme-decklin-tty))
       (menu-bar-mode -1))))
 
 ;; If we're not a daemon, the color theme should be installed globally.
