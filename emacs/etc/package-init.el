edfc35774bed4a79276737b8a682bbf6945e71c5
Decklin Foster <decklin@red-bean.com>
2014-10-29 16:53:52 -0400

    Update repositories

--- emacs/etc/package-init.el
+++ emacs/etc/package-init.el
@@ -1,4 +1,5 @@
 (when (require-soft 'package)
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
   (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+  (add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
   (package-initialize))
