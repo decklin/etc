814ece7b3631b1ecb14ae7ff7b9fcb19f08c6b8a
Decklin Foster <decklin@red-bean.com>
2017-02-06 09:28:05 -0500

    C-c C-c to end emacsclient, why not try it

--- emacs/etc/bindings.el
+++ emacs/etc/bindings.el
@@ -41,3 +41,11 @@
 
 (with-eval-after-load 'info
   (define-key Info-mode-map (kbd "M-n") 'next-buffer))
+
+;; Server
+
+(add-hook 'server-switch-hook
+          (lambda ()
+            (when (current-local-map)
+              (use-local-map (copy-keymap (current-local-map))))
+            (local-set-key (kbd "C-c C-c") 'server-edit)))
